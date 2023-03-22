a2d894c36d2ec415fd20b93a8fd2cc7004b16700
Angel Reyna <rugile@protonmail.com>
2023-03-22 21:35:21 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
+import { Link, Route, Routes } from "react-router-dom";
+import Cart from "./components/cart/Cart";
+import Home from "./components/home/Home";
 import ProductList from "./components/productList/ProductList";
 
 function App() {
   return (
-    <div>
-      <ProductList />
-
-    </div>
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+          <Link to="/products">Products</Link>
+          </li>
+          <li>
+          <Link to="/cart">Cart</Link>
+          </li>
+        </ul>
+      </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<ProductList />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+    </>
   );
 }
 
---
+++ src/components/home/Home.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
+
+// import { useState } from "react"
+
+// import Slider from "react-slick"
+// import { FaArrowRight, FaArrowLeft } from "react-icons/fa";
+// import products from '../data/data';
+
+
+// const Home = () => {
+
+//     // const settings = {
+//     //     infinite: true,
+//     //     lazyload: true,
+//     //     speed: 300,
+//     //     slidesToShow: 3,
+//     //     centerMode: true,
+//     //     centerPadding: 0
+//     // };
+
+//   return (
+
+//     <div className="Home"> Home
+//     <Slider>
+//         {images.map((img, idx) => (
+
+//             <div>
+//                 <img 
+//                 src={img} 
+//                 alt={img} />
+//             </div>
+//         ))}
+//     </Slider>
+//     </div>
+//   );
+// }
+
+// export default Home
