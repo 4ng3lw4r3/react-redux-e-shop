206c520a28dcfc20e27becb9a00f69525467f591
Angel Reyna <rugile@protonmail.com>
2023-03-24 13:45:11 +0200

    nav

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,30 +1,52 @@
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import CartIcon from "../icons";
+
 
 const Navbar = () => {
 
+    console.log(
+        useSelector((store) => {
+        console.log(store);
+    })
+    );
 
 
     return (
         <>
-            <nav>
+            <nav className="nav">
+
+                <Link to="/" className="site-title">Angel Dust</Link>
+
                 <div>
-                    <h1>Navbar</h1>
-                <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/products">Products</Link>
-                    </li>
-                    <li>
-                        <Link to="/cart">Cart</Link>
-                    </li>
-                </ul>
+                    <div className="nav-center">
+                        {/* <h3>Miwau</h3> */}
+                        <div className="nav-container">
+                        <CartIcon />
+                        <div className="amount-container">
+                            <p className='total-amount'>0</p>
+                            </div>
+                        </div>
+                    </div>
+
+
+
+                    <ul>
+                        <li className="active">
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/products">Products</Link>
+                        </li>
+                        <li>
+                            <Link to="/cart">Cart</Link>
+                        </li>
+                    </ul>
                 </div>
 
             </nav>
         </>
     );
-}
+};
 
 export default Navbar
